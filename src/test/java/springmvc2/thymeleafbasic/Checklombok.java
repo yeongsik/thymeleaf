ccfa11a11462ff7d04ed32335e7920f29e91276d
LAPTOP-N91UPOS7\dudtl <youngsik0266@gmail.com>
2022-03-22 21:47:37 +0900

    Initial commit

---
+++ src/test/java/springmvc2/thymeleafbasic/Checklombok.java
@@ -0,0 +1,13 @@
+package springmvc2.thymeleafbasic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Checklombok {
+
+    private String test;
+    private String lombok;
+
+}
